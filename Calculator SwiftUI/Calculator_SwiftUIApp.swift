58cb8079577458c5c16f0452952641bbae489252
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-09-19 22:30:42 +0200

    Initial Commit

---
+++ Calculator SwiftUI/Calculator_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_SwiftUIApp.swift
+//  Calculator SwiftUI
+//
+//  Created by Andrew on 9/19/20.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calculator SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Calculator SwiftUI
+//
+//  Created by Andrew on 9/19/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
