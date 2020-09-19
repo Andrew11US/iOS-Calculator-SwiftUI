214c6f14e7647968a904b13ec86be88d0edfb745
Andrii Halabuda <andrii.halabuda@gmail.com>
2020-09-19 23:20:22 +0200

    Added buttons layout

--- Calculator SwiftUI/ContentView.swift
+++ Calculator SwiftUI/ContentView.swift
@@ -8,15 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
+    var buttons = [
+        ["7", "8", "9", "X"],
+        ["4", "5", "6", "X"],
+        ["1", "2", "3", "X"],
+        ["0", ".", ".", "="],
+    ]
+    
     var body: some View {
-        VStack {
-            HStack {
-                ForEach(["1", "2", "3"], id: \.self) { button in
-                    Text(button)
-                        .font(.system(size: 32, weight: .bold, design: .default))
+        ZStack(alignment: .bottom) {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack(spacing: 10) {
+                HStack(alignment: .center) {
+                    Spacer()
+                    Text("42")
+                        .foregroundColor(.white)
+                        .font(.system(size: 72))
+                }.padding()
+                ForEach(buttons, id: \.self) { row in
+                    HStack(spacing: 12) {
+                        ForEach(row, id: \.self) { button in
+                            Text(button)
+                                .font(.system(size: 32, weight: .bold, design: .default))
+                                .frame(width: getButtonWidth(), height: getButtonWidth(), alignment: .center)
+                                .foregroundColor(.white)
+                                .background(Color.yellow)
+                                .cornerRadius(getButtonWidth())
+                        }
+                    }
                 }
-            }
+            }.padding(.bottom)
         }
+    }
+    
+    func getButtonWidth() -> CGFloat {
+        (UIScreen.main.bounds.width - 5 * 12) / 4
     }
 }
 
